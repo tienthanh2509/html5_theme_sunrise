0c0df9eb18d465cbd98c6787f10e00929a33aed3
Phạm Tiến Thành <tienthanh.dqc@gmail.com>
2016-05-05 19:46:27 +0700

    Dirty update

--- public/index.php
+++ public/index.php
@@ -5,21 +5,26 @@ require('../vendor/autoload.php');
 $app = new Silex\Application();
 $app['debug'] = true;
 
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', 1);
 
+// Register the monolog logging service
+//$app->register(new Silex\Provider\MonologServiceProvider(), array(
+//  'monolog.logfile' => 'php://stderr',
+//));
 // Register view rendering
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => dirname(__DIR__).'/application/views',
+    'twig.path' => dirname(__DIR__) . '/application/views',
+    'twig.options' => [
+        'strict_variables' => FALSE
+    ]
 ));
 
 // Our web handlers
 
 $app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.html.twig');
+//  $app['monolog']->addDebug('logging output.');
+    return $app['twig']->render('index.html.twig');
 });
 
 $app->run();
